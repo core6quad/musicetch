028656987e32960d9b576897c5fee5ef93e5bcdf
vasily <vasily@MacBook-Pro-vasily.local>
2025-04-30 20:05:12 +0200

    fixes

--- src/index.js
+++ src/index.js
@@ -28,14 +28,11 @@ const createWindow = () => {
   mainWindow.webContents.openDevTools();
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+
 app.whenReady().then(() => {
   createWindow();
 
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
+
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
@@ -43,9 +40,8 @@ app.whenReady().then(() => {
   });
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+
+
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
@@ -77,13 +73,6 @@ const walkMusicDir = (dir) => {
 
   return walk(dir);
 };
-
-// ipcMain.handle('select-folder', async () => {
-//   const result = await dialog.showOpenDialog({ properties: ['openDirectory'] });
-//   if (result.canceled) return {};
-//   const folderPath = result.filePaths[0];
-//   return walkMusicDir(folderPath);
-// });
 
 const configPath = path.join(app.getPath('userData'), 'config.json');
 
@@ -122,5 +111,3 @@ ipcMain.handle('reset-folder', () => {
 
 
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
